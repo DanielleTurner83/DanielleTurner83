cdc7c2a42183e42a2e9e876dbc643131f120d1da
Danielle Turner <danielle.2217.turner@gmail.com>
2025-08-13 18:15:49 -0500

    Create WN10-CC-000145.ps1

---
+++ STIG/github.com/WN10-CC-000145.ps1
@@ -0,0 +1,31 @@
+<#
+.SYNOPSIS
+    This PowerShell script ensures that Users must be prompted for a password on resume from sleep (on battery).
+
+.NOTES
+    Author          : Danielle Turner
+    LinkedIn        : linkedin.com/in/danielle-turner7/
+    GitHub          : github.com/DanielleTurner83
+    Date Created    : 2025-08-13
+    Last Modified   : 2025-08-13
+    Version         : 1.0
+    CVEs            : N/A
+    Plugin IDs      : N/A
+    STIG-ID         : WN10-CC-000145
+
+.TESTED ON
+    Date(s) Tested  : 
+    Tested By       : 
+    Systems Tested  : 
+    PowerShell Ver. : 
+
+.USAGE
+    Put any usage instructions here.
+    Example syntax:
+    PS C:\> .\__remediation_template(STIG-ID-WN10-AU-000500).ps1 
+#>
+
+
+# Create the registry path if it does not exist, then set the value
+New-Item -Path "HKLM:\SOFTWARE\Policies\Microsoft\Power\PowerSettings\0e796bdb-100d-47d6-a2d5-f7d2daa51f51" -Force | Out-Null
+Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Power\PowerSettings\0e796bdb-100d-47d6-a2d5-f7d2daa51f51" -Name "DCSettingIndex" -Type DWord -Value 1
