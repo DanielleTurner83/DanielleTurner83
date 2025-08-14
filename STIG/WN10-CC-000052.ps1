a4e075aa6bf65200ef0e164338cd02d1c82949c4
Danielle Turner <danielle.2217.turner@gmail.com>
2025-08-14 16:06:19 -0500

    Update WN10-CC-000052.ps1

--- STIG/WN10-CC-000052.ps1
+++ STIG/WN10-CC-000052.ps1
@@ -1,1 +1,30 @@
+<#
+.SYNOPSIS
+    This PowerShell script forces Windows10 to be configured to prioritize ECC Curves with longer key lenghts first .
 
+.NOTES
+    Author          : Danielle Turner
+    LinkedIn        : linkedin.com/in/danielle-turner7/
+    GitHub          : github.com/DanielleTurner83
+    Date Created    : 2025-08-14
+    Last Modified   : 2025-08-14
+    Version         : 1.0
+    CVEs            : N/A
+    Plugin IDs      : N/A
+    STIG-ID         : WN10-CC-000052 
+
+.TESTED ON
+    Date(s) Tested  : 
+    Tested By       : 
+    Systems Tested  : 
+    PowerShell Ver. : 
+
+.USAGE
+    Put any usage instructions here.
+    Example syntax:
+    PS C:\> .\__remediation_template(STIG-ID-WN10-CC-000052).ps1 
+#>
+
+# Must Run as administrator
+
+New-Item -Path "HKLM:\SOFTWARE\Policies\Microsoft\Cryptography\Configuration\SSL\00010002" -Force | Out-Null; Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Cryptography\Configuration\SSL\00010002" -Name "EccCurves" -Value @("NistP384","NistP256") -Type MultiString
